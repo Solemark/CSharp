2fe4c143eb620be51e2f373d59ab939bf868a438
Solemark <larco9mason@gmail.com>
2023-06-19 17:54:06 +1000

    fix typo String -> string

--- ConsoleApps/src/MultiplicationTables.cs
+++ ConsoleApps/src/MultiplicationTables.cs
@@ -4,7 +4,7 @@ public class MultiplicationTables
     public static List<string> run(int number, int max)
     {
         List<string> output = new List<string>();
-        String value = "";
+        string value = "";
         for (int i = 0; i <= max; i++)
         {
             value = String.Format("{0} x {1} = {2}", number, i, (number * i));
