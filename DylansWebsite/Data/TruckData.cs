7dcb23b2364768027e05327b4c98894dfd4bee7a
Solemark <larco9mason@gmail.com>
2023-07-02 18:10:56 +1000

    rename Vehicle to VehicleItem

--- DylansWebsite/Data/TruckData.cs
+++ DylansWebsite/Data/TruckData.cs
@@ -6,7 +6,7 @@ public static class TruckData
         return new Truck[] {
             new Truck("Small Truck", "Small truck details", "images/small_truck.webp"),
             new Truck("Medium Truck", "Medium truck details", "images/medium_truck.jpg"),
-            new Truck("Big Truck", "Big truck details", "images/big_truck.webp")
+            new Truck("Big Truck", "Big truck details", "images/big_truck.webp"),
         };
     }
 }
