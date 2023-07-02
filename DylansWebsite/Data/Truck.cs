356b810df05fe51668122116e75c174f116c94b6
Solemark <larco9mason@gmail.com>
2023-07-02 18:15:42 +1000

    updated Truck getter and setter methods

--- DylansWebsite/Data/Truck.cs
+++ DylansWebsite/Data/Truck.cs
@@ -1,29 +1,14 @@
 namespace DylansWebsite.Data;
 public class Truck
 {
-    private string title;
-    private string details;
-    private string image;
+    public string title { get; set; }
+    public string details { get; set; }
+    public string image { get; set; }
 
     public Truck(string title = "", string details = "", string image = "")
     {
         this.title = title;
         this.details = details;
         this.image = image;
-    }
-
-    public string getTitle()
-    {
-        return this.title;
-    }
-
-    public string getDetails()
-    {
-        return this.details;
-    }
-
-    public string getImage()
-    {
-        return this.image;
     }
 }
