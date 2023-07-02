f0abb1933e3923972a8b72a658f049725cd10fe5
Solemark <larco9mason@gmail.com>
2023-07-02 15:47:14 +1000

    update website to use components for vehicle details

---
+++ DylansWebsite/Data/Truck.cs
@@ -0,0 +1,30 @@
+namespace DylansWebsite.Data;
+public class Truck
+{
+    private string title;
+    private string details;
+    private string image;
+
+    public Truck(string title = "", string details = "", string image = "")
+    {
+        this.title = title;
+        this.details = details;
+        this.image = image;
+    }
+
+    public string getTitle()
+    {
+        return this.title;
+    }
+
+    public string getDetails()
+    {
+        return this.details;
+    }
+
+    public string getImage()
+    {
+        return this.image;
+    }
+
+}
---
+++ DylansWebsite/Data/TruckData.cs
@@ -0,0 +1,12 @@
+namespace DylansWebsite.Data;
+public static class TruckData
+{
+    public static Truck[] getData()
+    {
+        return new Truck[] {
+            new Truck("Small Truck", "Small truck details", "images/small_truck.webp"),
+            new Truck("Medium Truck", "Medium truck details", "images/medium_truck.jpg"),
+            new Truck("Big Truck", "Big truck details", "images/big_truck.webp")
+        };
+    }
+}
